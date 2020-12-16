3120512997f3395d4d4ac056fe418429a806731e
George <george.hill@crowncommercial.gov.uk>
2020-12-16 16:28:17 +0000

    create_service.py: Black formatting, and adding option to create service with parameters

--- scripts/create_service.py
+++ scripts/create_service.py
@@ -7,31 +7,45 @@ from cf_common import get_space_guid, cf_client_initialise
 def parse_arguments():
     description = "Arguments for create_service.py"
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("-instance-name",
-                        help="The instance name to give to the service",
-                        dest="instance_name",
-                        required=True)
-    parser.add_argument("-organisation",
-                        help="The organisation to create resources within",
-                        dest="organisation",
-                        required=True)
-    parser.add_argument("-service-plan",
-                        help="The name of the service plan to create the service for",
-                        dest="service_plan",
-                        required=True)
-    parser.add_argument("-space",
-                        help="The name of the space to create resources for",
-                        dest="space",
-                        required=True)
+    parser.add_argument(
+        "-instance-name",
+        help="The instance name to give to the service",
+        dest="instance_name",
+        required=True,
+    )
+    parser.add_argument(
+        "-organisation",
+        help="The organisation to create resources within",
+        dest="organisation",
+        required=True,
+    )
+    parser.add_argument(
+        "-parameters",
+        help="The paramters to assign to the created service key",
+        dest="parameters",
+        required=False,
+    )
+    parser.add_argument(
+        "-service-plan",
+        help="The name of the service plan to create the service for",
+        dest="service_plan",
+        required=True,
+    )
+    parser.add_argument(
+        "-space",
+        help="The name of the space to create resources for",
+        dest="space",
+        required=True,
+    )
     return parser.parse_args()
 
 
 def check_service_plan_exists(client, service_plan):
     service_plan_exists = False
     for plan in client.v2.service_plans:
-        if plan['entity']['name'] == service_plan:
+        if plan["entity"]["name"] == service_plan:
             service_plan_exists = True
-            service_plan_guid = plan['metadata']['guid']
+            service_plan_guid = plan["metadata"]["guid"]
 
     if service_plan_exists:
         return service_plan_guid
@@ -40,36 +54,101 @@ def check_service_plan_exists(client, service_plan):
         exit(1)
 
 
-def create_service(client, instance_name, service_plan_guid, space_guid, threshold=20):
+def create_service_with_parameters(
+    client, instance_name, service_plan_guid, space_guid, parameters, threshold=20
+):
     try:
-        client.v2.service_instances.create(space_guid=space_guid, instance_name=instance_name, plan_guid=service_plan_guid, accepts_incomplete=True)
+        client.v2.service_instances.create(
+            space_guid=space_guid,
+            instance_name=instance_name,
+            plan_guid=service_plan_guid,
+            accepts_incomplete=True,
+            parameters=parameters,
+        )
         print("Service instance creation in progress for: {}".format(instance_name))
         for i in range(threshold):
             i = i + 1
-            service_created = check_service_status(client=client, instance_count=i, instance_name=instance_name)
+            service_created = check_service_status(
+                client=client, instance_count=i, instance_name=instance_name
+            )
+            if service_created:
+                break
+            if i >= 40:
+                print("Timed out waiting for {} to reach state".format(instance_name))
+                exit(1)
+    except Exception as e:
+        print(
+            "Received exception when creating service instance: {}: {}".format(
+                instance_name, e
+            )
+        )
+        exit(1)
+
+
+def create_service(client, instance_name, service_plan_guid, space_guid, threshold=20):
+    try:
+        client.v2.service_instances.create(
+            space_guid=space_guid,
+            instance_name=instance_name,
+            plan_guid=service_plan_guid,
+            accepts_incomplete=True,
+        )
+        print("Service instance creation in progress for: {}".format(instance_name))
+        for i in range(threshold):
+            i = i + 1
+            service_created = check_service_status(
+                client=client, instance_count=i, instance_name=instance_name
+            )
             if service_created:
                 break
             if i >= 20:
                 print("Timed out waiting for {} to reach state".format(instance_name))
                 exit(1)
     except Exception as e:
-        print("Received exception when creating service instance: {}: {}".format(instance_name, e))
+        print(
+            "Received exception when creating service instance: {}: {}".format(
+                instance_name, e
+            )
+        )
 
 
-def check_service_status(client, instance_count, instance_name, desired_type='create', desired_state='succeeded', threshold=20):
+def check_service_status(
+    client,
+    instance_count,
+    instance_name,
+    desired_type="create",
+    desired_state="succeeded",
+    threshold=20,
+):
     for service in client.v2.service_instances:
-        if service['entity']['name'] == instance_name:
-            print("Polling to check status of {}, attempt {} of {}".format(instance_name, instance_count, threshold))
-            if service['entity']['last_operation']['type'] == desired_type:
-                if service['entity']['last_operation']['state'] == desired_state:
-                    print('{} has reached the desired state of {}'.format(instance_name, desired_state))
+        if service["entity"]["name"] == instance_name:
+            print(
+                "Polling to check status of {}, attempt {} of {}".format(
+                    instance_name, instance_count, threshold
+                )
+            )
+            if service["entity"]["last_operation"]["type"] == desired_type:
+                if service["entity"]["last_operation"]["state"] == desired_state:
+                    print(
+                        "{} has reached the desired state of {}".format(
+                            instance_name, desired_state
+                        )
+                    )
                     return True
                 else:
-                    print('{} is currently in state {}'.format(instance_name, service['entity']['last_operation']['state']))
-                    os.system('sleep 60')
+                    print(
+                        "{} is currently in state {}".format(
+                            instance_name, service["entity"]["last_operation"]["state"]
+                        )
+                    )
+                    os.system("sleep 60")
                     return False
             else:
-                print("Expected last operation to be {} but got {}, exiting...".format(desired_type, service['last_operation']['type']))
+                print(
+                    "Expected last operation to be {} but got {}, exiting...".format(
+                        desired_type, service["last_operation"]["type"]
+                    )
+                )
                 exit(1)
 
 
@@ -78,10 +157,32 @@ args = parse_arguments()
 instance_name = args.instance_name
 organisation = args.organisation
 organisation_space = args.space
+parameters = args.parameters
 service_plan = args.service_plan
 
 # Call methods
 client = cf_client_initialise()
-space_guid = get_space_guid(client=client, organisation=organisation, organisation_space=organisation_space)
+space_guid = get_space_guid(
+    client=client, organisation=organisation, organisation_space=organisation_space
+)
 service_plan_guid = check_service_plan_exists(client=client, service_plan=service_plan)
-create_service(client=client, instance_name=instance_name, service_plan_guid=service_plan_guid, space_guid=space_guid)
+if parameters:
+    try:
+        service_parameters = json.loads(parameters)
+        create_service_with_parameters(
+            client=client,
+            instance_name=instance_name,
+            parameters=service_parameters,
+            service_plan_guid=service_plan_guid,
+            space_guid=space_guid,
+        )
+    except json.decoder.JSONDecodeError as e:
+        print(f"Could not load service parameters {parameters}: {e}")
+        exit(1)
+else:
+    create_service(
+        client=client,
+        instance_name=instance_name,
+        service_plan_guid=service_plan_guid,
+        space_guid=space_guid,
+    )
